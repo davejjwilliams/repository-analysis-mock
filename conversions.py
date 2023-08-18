00a6efcd0538819cbc9e8ace1e922af2c540694c
davejjwilliams <davejjwilliams@gmail.com>
2023-08-18 13:58:53 +0100

    Conversion & Main Update

--- conversions.py
+++ conversions.py
@@ -1,5 +1,8 @@
 def km_to_m(num):
     return float(num)/1000
 
+def miles_to_km(num):
+    return float(num)*1.60934
+
 def in_to_cm(num):
     return num*2.54
--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from conversions import *
 
 num = 5
 
+print ("Maths:")
 print("Factorial of {} = {}".format(num, fact(num)))
-print("Square of {} =".format(num, sq(num)))
+print("Square of {} = {}".format(num, sq(num)))
+print("{} is even? {}".format(num, even(num)))
+print("{} is odd? {}".format(num, odd(num)))
+print()
+print("Conversions:")
 print("{} kilometres is {} metres".format(num, km_to_m(num)))
 print("{} inches is {} centimetres".format(num, in_to_cm(num)))
-
-print("")
